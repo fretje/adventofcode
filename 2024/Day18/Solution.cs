2da8cbbc636d7d1a17d38cc1e7c580ad05ea1997
fretje <github@fretje.be>
2024-12-18 18:07:30 +0100

    day 18: add binary search

--- 2024/Day18/Solution.cs
+++ 2024/Day18/Solution.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics;
-
-namespace AdventOfCode.Y2024.Day18;
+﻿namespace AdventOfCode.Y2024.Day18;
 
 [ProblemName("RAM Run")]
 class Solution : Solver 
@@ -9,70 +7,68 @@ class Solution : Solver
     private const int Height = 71;
     private const int FirstBytes = 1024;
 
-    public object PartOne(string[] lines) 
-    {
-        var bytes = ParseInput(lines);
-        var grid = GetGrid(Width, Height, bytes[..FirstBytes]);
-        return GetMinimumSteps(grid) ?? 0;
-    }
+    public object PartOne(string[] lines) => 
+        GetGrid(lines.ParseInput()[..FirstBytes]).GetMinimumSteps() ?? 0;
 
     public object PartTwo(string[] lines)
     {
-        var bytes = ParseInput(lines);
-        var grid = GetGrid(Width, Height, bytes[..FirstBytes]);
-        foreach (var b in bytes[FirstBytes..])
+        var bytes = lines.ParseInput();
+        var low = FirstBytes;
+        var hi = bytes.Length - 1;
+        while (low < hi)
         {
-            grid.SetValueAt(b, '#');
-            if (GetMinimumSteps(grid) is null)
+            var mid = (low + hi) / 2;
+            if (GetGrid(bytes[..(mid + 1)]).GetMinimumSteps() is null)
             {
-                return $"{b.Col},{b.Row}";
+                hi = mid;
+            }
+            else
+            {
+                low = mid + 1;
             }
         }
-        throw new UnreachableException();
+        return $"{bytes[low].Col},{bytes[low].Row}";
     }
 
-    private static Pos[] ParseInput(string[] lines) =>
-        [.. lines.Select(l => l.Split(",")).Select(l => new Pos(int.Parse(l[0]), int.Parse(l[1])))];
-
-    private static char[][] GetGrid(int width, int height, Pos[] firstBytes) => 
-        Enumerable.Range(0, height)
-            .Select(row => Enumerable.Range(0, width)
+    public static char[][] GetGrid(Pos[] firstBytes) => 
+        Enumerable.Range(0, Height)
+            .Select(row => Enumerable.Range(0, Width)
                 .Select(col => firstBytes.Contains(new Pos(col, row)) ? '#' : '.')
                 .ToArray())
             .ToArray();
+}
 
-    private static int? GetMinimumSteps(char[][] grid)
+static class Extensions
+{
+    public static Pos[] ParseInput(this string[] lines) =>
+        [.. lines.Select(l => l.Split(",")).Select(l => new Pos(int.Parse(l[0]), int.Parse(l[1])))];
+
+
+    public static int? GetMinimumSteps(this char[][] grid)
     {
-        List<int> solutions = [];
-        Queue<(Pos Pos, int Steps)> queue = [];
-        Dictionary<Pos, int> seen = [];
         var start = new Pos(0, 0);
         var end = new Pos(grid[0].Length - 1, grid.Length - 1);
+        Queue<(Pos Pos, int Steps)> queue = [];
         queue.Enqueue((start, 0));
-        seen[start] = 0;
+        HashSet<Pos> seen = [start];
         while (queue.Count != 0)
         {
             var (pos, price) = queue.Dequeue();
-            if (!grid.Contains(pos) || grid.ValueAt(pos) == '#')
-            {
-                continue;
-            }
-            if (pos == end)
-            {
-                solutions.Add(price);
-                continue;
-            }
             foreach (var dir in Directions.All)
             {
-                var newPos = pos + dir;
-                var newPrice = price + 1;
-                if (!seen.TryGetValue(newPos, out var seenPrice) || seenPrice > newPrice)
+                var next = pos + dir;
+                if (!grid.Contains(next) || grid.ValueAt(next) == '#' || seen.Contains(next))
                 {
-                    seen[newPos] = price;
-                    queue.Enqueue((newPos, newPrice));
+                    continue;
                 }
+                if (next == end)
+                {
+                    return price + 1;
+                }
+                seen.Add(next);
+                queue.Enqueue((next, price + 1));
             }
         }
-        return solutions.Count == 0 ? null : solutions.Min();
+        return null;
     }
 }
