fa522a980716cb2d2f1265d4263b893b7816641c
fretje <github@fretje.be>
2025-12-03 18:39:29 +0100

    2025 day 3

--- 2025/Day03/Solution.cs
+++ 2025/Day03/Solution.cs
@@ -1,15 +1,37 @@
-namespace AdventOfCode.Y2025.Day03;
+﻿namespace AdventOfCode.Y2025.Day03;
 
 [ProblemName("Lobby")]
 class Solution : Solver 
-{            
-    public object PartOne(string[] lines) 
-    {
-        return 0;
-    }
+{
+    public object PartOne(string[] lines) => Calculate(lines, 2);
 
-    public object PartTwo(string[] lines) 
-    {
-        return 0;
-    }
+    public object PartTwo(string[] lines) => Calculate(lines, 12);
+
+    private static long Calculate(string[] lines, int numberOfBatteries) =>
+        lines.Sum(line =>
+        {
+            var bank = line.Select(x => x - '0').ToArray();
+            int[] maxValue = new int[numberOfBatteries];
+            int[] maxIndex = new int[numberOfBatteries];
+            var joltage = 0L;
+
+            for (int batteryIndex = 0; batteryIndex < numberOfBatteries; batteryIndex++)
+            {
+                for (int bankIndex = batteryIndex == 0 ? 0 : maxIndex[batteryIndex-1] + 1; 
+                    bankIndex < bank.Length - numberOfBatteries + batteryIndex + 1; 
+                    bankIndex++)
+                {
+                    if (bank[bankIndex] <= maxValue[batteryIndex])
+                    {
+                        continue;
+                    }
+                    maxValue[batteryIndex] = bank[bankIndex];
+                    maxIndex[batteryIndex] = bankIndex;
+                }
+
+                joltage += maxValue[batteryIndex] * (long)Math.Pow(10, numberOfBatteries - batteryIndex - 1);
+            }
+
+            return joltage;
+        });
 }
