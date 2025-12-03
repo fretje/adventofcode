8a5183a57ed0ce00826cde6e09595f95a6696676
fretje <github@fretje.be>
2025-12-03 14:59:08 +0100

    2025 day 1

---
+++ 2025/Day01/Solution.cs
@@ -0,0 +1,43 @@
+﻿namespace AdventOfCode.Y2025.Day01;
+
+[ProblemName("Secret Entrance")]
+class Solution : Solver 
+{            
+    public object PartOne(string[] lines) 
+    {
+        int dial = 50;
+        int zeroCount = 0;
+        foreach (var clicks in lines.Select(line => int.Parse(line[1..]) * (line[0] == 'L' ? -1 : 1)))
+        {
+            dial = (dial + clicks) % 100;
+            if (dial == 0)
+            {
+                zeroCount++;
+            }
+        }
+
+        return zeroCount;
+    }
+
+    public object PartTwo(string[] lines) 
+    {
+        int dial = 50;
+        int zeroCount = 0;
+        foreach (var clicks in lines.Select(line => int.Parse(line[1..]) * (line[0] == 'L' ? -1 : 1)))
+        {
+            var times = Math.Abs(clicks);
+            for (int i = 0; i < times; i++)
+            {
+                dial = clicks > 0 
+                    ? (dial == 99 ? 0 : dial + 1) 
+                    : (dial == 0 ? 99 : dial - 1);
+                if (dial == 0)
+                {
+                    zeroCount++;
+                }
+            }
+        }
+
+        return zeroCount;
+    }
+}
---
+++ 2025/SplashScreen.cs
@@ -0,0 +1,51 @@
+namespace AdventOfCode.Y2025;
+
+internal class SplashScreenImpl : SplashScreen
+{
+    public void Show()
+    {
+        var color = Console.ForegroundColor;
+        Write(0xcc00, false, "           ▄█▄ ▄▄█ ▄ ▄ ▄▄▄ ▄▄ ▄█▄  ▄▄▄ ▄█  ▄▄ ▄▄▄ ▄▄█ ▄▄▄\n           █▄█ █ █ █ █ █▄█ █ █ █   █ █ █▄ ");
+        Write(0xcc00, false, " █  █ █ █ █ █▄█\n           █ █ █▄█ ▀▄▀ █▄▄ █ █ █▄  █▄█ █   █▄ █▄█ █▄█ █▄▄  λy.2025\n            \n    ");
+        Write(0xcc00, false, "       ");
+        Write(0xffffff, false, "''    .  ____ ..'.   .  ");
+        Write(0xffff66, true, "* ");
+        Write(0xffffff, false, ". '     '  .   ");
+        Write(0xff9900, false, "<");
+        Write(0xffffff, false, "o   .         \n           ________/");
+        Write(0x999999, false, "O___");
+        Write(0xffffff, false, "\\__________");
+        Write(0xff0000, false, "|");
+        Write(0xffffff, false, "_________________O______  ");
+        Write(0xcccccc, false, " 1 ");
+        Write(0xffff66, false, "**\n           ");
+        Write(0x666666, false, "   _______||_________                                   \n              | _@__ || _o_    |_ _________");
+        Write(0x666666, false, "________________   ");
+        Write(0xcccccc, false, " 2 ");
+        Write(0x666666, false, "**\n              |_&_%__||_oo__^=_[ \\|     _    .. .. ..     |        \n                             ");
+        Write(0x666666, false, "   \\_]__--|_|___[]_[]_[]__//_|   ");
+        Write(0xcccccc, false, " 3 ");
+        Write(0x666666, false, "**\n           ");
+        Write(0x333333, false, "                               ____________//___        \n                                          |");
+        Write(0x333333, false, " \\        // @@|   ");
+        Write(0x666666, false, " 4\n           ");
+        Write(0x333333, false, "                               |_[#]_@@__//_@@@|        \n                                           ");
+        Write(0x333333, false, "                   ");
+        Write(0x666666, false, " 5\n                                                                   \n                             ");
+        Write(0x666666, false, "                                  6\n                                                                ");
+        Write(0x666666, false, "   \n                                                               7\n                               ");
+        Write(0x666666, false, "                                    \n                                                               ");
+        Write(0x666666, false, "8\n                                                                   \n                              ");
+        Write(0x666666, false, "                                 9\n                                                                 ");
+        Write(0x666666, false, "  \n                                                              10\n                                ");
+        Write(0x666666, false, "                                   \n                                                              11");
+        Write(0x666666, false, "\n                                                                   \n                               ");
+        Write(0x666666, false, "                               12\n           \n");
+        
+        Console.ForegroundColor = color;
+        Console.WriteLine();
+    }
+
+    private static void Write(int rgb, bool bold, string text) =>
+        Console.Write($"\u001b[38;2;{(rgb>>16)&255};{(rgb>>8)&255};{rgb&255}{(bold ? ";1" : "")}m{text}");
+}
